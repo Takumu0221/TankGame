d5d5f07161308adc428f678e5daa39ccc03e3746
Takumu0221 <74670690+Takumu0221@users.noreply.github.com>
2020-11-30 22:47:43 +0900

    反射を一旦考慮しないように変更

--- TankGame.py
+++ TankGame.py
@@ -448,12 +448,14 @@ class Enemy(Tank):
         if self.JudgeAim(rad):
             return rad
 
+        """
         # 反射で狙えるか判定
         rad -= math.pi * 0.5
         parts = 18
         for rad in [rad + math.pi * x / parts for x in range(1, parts)]:
             if self.JudgeAim(rad):
                 return rad
+        """
 
         return 0
 
